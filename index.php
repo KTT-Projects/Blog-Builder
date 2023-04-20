ad5c06f4ac0d76dd78399e06a1acd379dbc229b0
SEACLUE <75559644+SEACLUE@users.noreply.github.com>
2023-04-20 14:49:39 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./images/logo.png">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./style/style.css">
+  <title>Blog Builder - a high quality and easy-to-use blog maker</title>
+</head>
+
+<body>
+  <img class="top" src="./images/top.jpeg" alt="Landing image">
+  <div class="top_gradient"></div>
+  <img class="top_logo" src="./images/logo_long.png" alt="Blog Builder long logo">
+  <div class="top_start">Get Started</div>
+  <img class="scroll" src="./images/scroll.png" alt="Scroll down">
+  <div class="scroll_text">Scroll Down</div>
+  <div class="main">
+    
+  </div>
+</body>
+
+</html>
